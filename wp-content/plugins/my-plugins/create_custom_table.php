22927e4ca5180c4c9ed7274579c0d0a332ba77b1
UnKnowMQ <buiminhquan300304@gmail.com>
2025-12-29 16:38:09 +0700

    feat:add custom role

--- wp-content/plugins/my-plugins/create_custom_table.php
+++ wp-content/plugins/my-plugins/create_custom_table.php
@@ -13,4 +13,6 @@ require_once plugin_dir_path(__FILE__) . 'includes/db.php';
 
 register_activation_hook(__FILE__, 'create_custom_table');
 register_activation_hook(__FILE__, 'create_custom_table2');
+register_activation_hook(__FILE__, 'add_contact_info');
 
+add_contact_info("name","email","test");
--- wp-content/themes/custom_theme/functions.php
+++ wp-content/themes/custom_theme/functions.php
@@ -1,6 +1,7 @@
-<?php 
+<?php
 
-function mytheme_enqueue_assets() {
+function mytheme_enqueue_assets()
+{
     wp_enqueue_style(
         'mytheme-style',
         get_stylesheet_uri(),
@@ -11,19 +12,55 @@ function mytheme_enqueue_assets() {
 
 add_action('wp_enqueue_scripts', 'mytheme_enqueue_assets');
 
-function create_projects_post_type() {
+function create_projects_post_type()
+{
     register_post_type('projects', [
         'labels' => [
             'name' => 'Projects',
             'singular_name' => 'Project'
         ],
         'public' => true,
-        'has_archive' => true,  
+        'has_archive' => true,
         'rewrite' => ['slug' => 'projects'],
         'show_in_rest' => true,
     ]);
 }
 add_action('init', 'create_projects_post_type');
 
-remove_action('init', 'create_custom_table');
-?>
+if (! get_role('hr')) {
+    add_role('hr', __('HR'), array(
+        'manage_users' => true,
+        'read' => true,
+        'create_users' => true,
+        'edit_users' => true,
+    ));
+}
+if (! get_role('CM')) {
+    add_role('CM', __('Content Manager'), array(
+        'edit_posts' => true,
+        'edit_others_posts' => true,
+        'publish_posts' => true,
+        'delete_posts' => true,
+        'delete_others_posts' => true,
+        'edit_pages' => true,
+        'edit_others_pages' => true,
+        'publish_pages' => true,
+        'delete_pages' => true,
+        'delete_others_pages' => true,
+        'upload_files' => true,
+        'edit_files' => true,
+        'moderate_comments' => true,
+        'read' => true,
+    ));
+}
+// function update_role_cap($caps, $role)
+// {
+//     $role = get_role($role);
+//     if (!$role) return;
+//     else {
+//         for ($i = 0; $i < sizeof($caps); $i++) {
+//             $role->add_cap($caps[$i]);
+//         }
+//     }
+// }
+// update_role_cap(['create_users', 'list_users', 'edit_users', 'read'], 'hr');
